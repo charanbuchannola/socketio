74f5d3b86f642e7a89eefed3036b3aa9be92710a
charanbuchannola <buchannolacharan@gmail.com>
2025-02-21 12:20:07 +0530

    Add socket.io dependency and update connection handler syntax

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./src/app");
 
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-io.on("connection", (socket) => {
+io.on("connection", socket => {
   console.log("A user connected");
 });
 server.listen(3000, () => {
