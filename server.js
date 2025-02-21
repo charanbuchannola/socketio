6d6d2493d2d75ec168a2628196b9c314cd60dcad
charanbuchannola <buchannolacharan@gmail.com>
2025-02-21 12:33:22 +0530

    Enhance socket.io connection to handle incoming messages and broadcast chat messages

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const app = require("./src/app");
 
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-io.on("connection", socket => {
-  console.log("A user connected");
+io.on("connection", (socket) => {
+  socket.on("message", (data) => {
+    console.log(data);
+    io.emit("chat-message", data);
+  });
 });
 server.listen(3000, () => {
   console.log("Server started on http://localhost:3000");
